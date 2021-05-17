8caac02906b2edb83205bac04862cd1ad649435f
efrizalnf <ondabuzz9@gmail.com>
2021-05-17 18:34:41 +0700

    fix layout

--- app/src/main/java/id/zlz/ininews/adapter/NewsAdapter.kt
+++ app/src/main/java/id/zlz/ininews/adapter/NewsAdapter.kt
@@ -3,10 +3,12 @@ package id.zlz.ininews.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import id.zlz.ininews.R
 
 class NewsAdapter: RecyclerView.Adapter<NewsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from()
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news_vertical, parent, false)
+        return NewsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
